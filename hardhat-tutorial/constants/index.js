637d787c4af4354ee7f24fdd1407f66773e290ea
gisantiago <gisantiago@live.com>
2022-04-06 18:02:15 -0400

    Initial commit - backend complete

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS = "0x25B276fA1BB9d9aFB3B936c0ea8f3a2B0378b422";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
